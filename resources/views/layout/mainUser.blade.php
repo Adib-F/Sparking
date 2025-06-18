ede05a06e359f0799ea2f9ff308d4bae4d91eea8
Greyari <Greyari>
2025-06-18 15:38:50 +0700

    rabu-1 deploy

--- resources/views/layout/mainUser.blade.php
+++ resources/views/layout/mainUser.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{ asset('images/icon.png')}}" type="image/x-icon">
     <title>{{ $title }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,15 +13,18 @@
         rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.12.0/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css','resources/js/app.js'])
-    <link href="{{ asset('css_user/user_landing_page.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
     <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="icon" href="{{ secure_asset('images/icon.png')}}" type="image/x-icon">
+    <link href="{{ secure_asset('css/dashboard.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css_user/user_landing_page.css') }}" rel="stylesheet">
+
 </head>
 
 <body class="min-h-screen bg-gray-100">
