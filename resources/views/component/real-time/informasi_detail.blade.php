6f2548d8e96af99ea44439e50b097ad6165a7a78
Greyari <159263690+Greyari@users.noreply.github.com>
2025-07-01 10:18:00 +0700

    selasa 1

--- resources/views/component/real-time/informasi_detail.blade.php
+++ resources/views/component/real-time/informasi_detail.blade.php
@@ -129,7 +129,7 @@
 
         init() {
             const timestamp = Date.now();
-            this.streamUrl = `https://talking-walking-francisco-fastest.trycloudflare.com/clean_video_feed?camera_id=${this.cameraId}&subzona_id=${this.subzonaId}`;
+            this.streamUrl = `https://ride-cycles-edwards-matches.trycloudflare.com/clean_video_feed?camera_id=${this.cameraId}&subzona_id=${this.subzonaId}`;
             console.log("Starting clean stream:", this.streamUrl);
             const videoElement = document.getElementById(this.elementId);
             videoElement.src = this.streamUrl;
