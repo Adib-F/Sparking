8f4b2b66afe011457be29ad9cd0ca90a235d9adf
adib-f <fakhrimuhammadadib@gmail.com>
2025-07-04 13:46:05 +0700

    jumat

--- resources/views/component/real-time/informasi_detail.blade.php
+++ resources/views/component/real-time/informasi_detail.blade.php
@@ -129,7 +129,7 @@
 
         init() {
             const timestamp = Date.now();
-            this.streamUrl = `https://ride-cycles-edwards-matches.trycloudflare.com/clean_video_feed?camera_id=${this.cameraId}&subzona_id=${this.subzonaId}`;
+            this.streamUrl = `https://rw-dirt-again-wine.trycloudflare.comclean_video_feed?camera_id=${this.cameraId}&subzona_id=${this.subzonaId}`;
             console.log("Starting clean stream:", this.streamUrl);
             const videoElement = document.getElementById(this.elementId);
             videoElement.src = this.streamUrl;
